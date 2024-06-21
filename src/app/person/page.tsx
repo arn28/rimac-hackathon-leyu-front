55e37cd7f87424124771f49f4974f984f63a7c9d
arn28 <arnold.dev.me@gmail.com>
2024-06-21 17:17:22 -0500

    feat: added ui components and theme provider

---
+++ src/app/person/page.tsx
@@ -0,0 +1,16 @@
+import { Button } from '@nextui-org/button'
+export default function Person() {
+  return (
+    <main className='min-h-screen flex-col items-center justify-between p-24 '>
+      <h1>Welcome ...</h1>
+      <div className='flex gap-4'>
+        <Button radius='full' color='primary' variant='solid'>
+          Solid
+        </Button>
+        <Button color='primary' variant='ghost'>
+          Ghost
+        </Button>
+      </div>
+    </main>
+  )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,41 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
+import { nextui } from '@nextui-org/theme'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/components/(button).js',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: '#f7052d',
       },
     },
   },
-  plugins: [],
-};
-export default config;
+  darkMode: 'class',
+  plugins: [
+    nextui({
+      layout: {
+        // disabledOpacity: '0.3', // opacity-[0.3]
+        radius: {
+          small: '2px', // rounded-small
+          medium: '4px', // rounded-medium
+          large: '6px', // rounded-large
+        },
+        borderWidth: {
+          small: '1px', // border-small
+          medium: '1px', // border-medium
+          large: '2px', // border-large
+        },
+      },
+      themes: {
+        light: {},
+        dark: {},
+      },
+    }),
+  ],
+}
+export default config
