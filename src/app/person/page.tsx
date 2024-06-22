258a26a345a4b22071b102962c0e0e692ec429fc
Agustinsn <agustinasn95@gmail.com>
2024-06-22 00:39:11 -0500

    DeployTest: * Se agregó la libreria AXIOS * Se consume API de aws

--- src/app/person/page.tsx
+++ src/app/person/page.tsx
@@ -1,4 +1,6 @@
+'use client'
 import { Button, ButtonGroup } from '@nextui-org/button'
+import { addClient } from './services/addClient.service'
 export default function Person() {
   return (
     <main className='min-h-screen flex-col items-center justify-between p-24 '>
@@ -8,7 +10,7 @@ export default function Person() {
           Solid
         </Button>
         <Button color='primary'>Button</Button>
-        <Button color='primary' variant='ghost'>
+        <Button color='primary' variant='ghost' onClick={() => {addClient()}}>
           Ghost
         </Button>
       </div>
---
+++ src/app/person/services/addClient.service.ts
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+interface Client{
+    firstName: string;
+    lastName: number;
+    email: string;
+}
+
+export const addClient = async () => {
+    console.log('addClient')
+    // todo: pasar la url a .env
+    axios({
+        method: 'post',
+        url: 'https://5txl3p0ghf.execute-api.us-west-2.amazonaws.com/addClient',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        data:{    
+            "email":"prueba123@abcmail.com",
+            "firstName":"Cliente",
+            "lastName":"DePrueba"
+        }
+    })
+}
