a40c220c43b7ec721db10459de5f8b26ba0368c6
Agustinsn <agustinasn95@gmail.com>
2024-06-22 01:28:56 -0500

    Test: Axios post

--- src/app/person/services/addClient.service.ts
+++ src/app/person/services/addClient.service.ts
@@ -10,21 +10,21 @@ export const addClient = async () => {
     console.log('addClient')
     // todo: pasar la url a .env
 
-    axios({
-        method:'get',
-        url:'https://5txl3p0ghf.execute-api.us-west-2.amazonaws.com/',
-    })
-
     //axios({
-    //    method: 'post',
-    //    url: 'https://5txl3p0ghf.execute-api.us-west-2.amazonaws.com/addClient',
-    //    headers: {
-    //        'Content-Type': 'application/json',
-    //    },
-    //    data:{    
-    //        "email":"prueba123@abcmail.com",
-    //        "firstName":"Cliente",
-    //        "lastName":"DePrueba"
-    //    }
+    //    method:'get',
+    //    url:'https://5txl3p0ghf.execute-api.us-west-2.amazonaws.com/',
     //})
+
+    axios({
+        method: 'post',
+        url: 'https://5txl3p0ghf.execute-api.us-west-2.amazonaws.com/addClient/',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        data:{    
+            "email":"prueba123@abcmail.com",
+            "firstName":"Cliente",
+            "lastName":"DePrueba"
+        }
+    })
 }
