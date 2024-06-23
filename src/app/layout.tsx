641908a93196f9ac0e87617832f1d4ad3d904d0d
arn28 <arnold.dev.me@gmail.com>
2024-06-22 23:20:21 -0500

    chore: order components and features files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import { ThemeSwitcher } from './components/ThemeSwitcher'
+import { ThemeSwitcher } from '../components/ThemeSwitcher'
 import { NextUIProviderTheme } from './NextUIProviderTheme'
 
 const inter = Inter({ subsets: ['latin'] })
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
 // import { useRef } from 'react'
 import { Button } from '@nextui-org/button'
 import { Image } from '@nextui-org/image'
-import { ArrowRightOutline } from './components/icons/ArrowRightOutline'
-import { PlussCircleSolid } from './components/icons/PlussCircleSolid'
-import { RimacIcon } from './components/icons/RimacIcon'
-import { FacebookIcon } from './components/icons/FacebookIcon'
-import { LinkedInIcon } from './components/icons/LinkedInIcon'
-import { YoutubeIcon } from './components/icons/YoutubeIcon'
+import { ArrowRightOutline } from '../components/icons/ArrowRightOutline'
+import { PlussCircleSolid } from '../components/icons/PlussCircleSolid'
+import { RimacIcon } from '../components/icons/RimacIcon'
+import { FacebookIcon } from '../components/icons/FacebookIcon'
+import { LinkedInIcon } from '../components/icons/LinkedInIcon'
+import { YoutubeIcon } from '../components/icons/YoutubeIcon'
 import Link from 'next/link'
-import ArrowButtonScroll from './components/ArrowButtonScroll'
+import ArrowButtonScroll from '../components/ArrowButtonScroll'
+import Footer from '@/features/Footer'
 
 export default function Home() {
   return (
@@ -130,43 +131,7 @@ export default function Home() {
           </a>
         </div>
       </section>
-      <footer className='bg-[#D9D9D9] p-16 flex flex-col items-center gap-8 '>
-        <div className=' flex items-center justify-between w-full '>
-          <RimacIcon />
-          <div className='flex gap-2'>
-            <a
-              className='cursor-pointer'
-              href='https://www.facebook.com/segurosrimac/'
-              target='_blank'
-            >
-              <FacebookIcon />
-            </a>
-            <a
-              className='cursor-pointer'
-              href='https://www.linkedin.com/company/rimac-seguros/'
-              target='_blank'
-            >
-              <LinkedInIcon />
-            </a>
-            <a
-              className='cursor-pointer'
-              href='https://www.youtube.com/user/SegurosRimac/'
-              target='_blank'
-            >
-              <YoutubeIcon />
-            </a>
-          </div>
-        </div>
-        <p className='text-[#5E6488]'>
-          Todos los derechos reservados. La información contenida en esta página
-          web es solo para fines informativos y no constituye asesoramiento
-          legal, financiero ni de seguros. Al utilizar este sitio, usted acepta
-          nuestros términos y condiciones. Para más detalles, consulte nuestras
-          políticas de privacidad y uso
-          <br />© 2024 RIMAC SEGUROS Y REASEGUROS. Todos los Derechos
-          Reservados.
-        </p>
-      </footer>
+      <Footer />
     </main>
   )
 }
